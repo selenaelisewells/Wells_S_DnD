f551f4e545aea4033b833d97c681cef4300343ab
Selena Wells <selena.elise.wells@gmail.com>
2020-05-29 12:06:04 -0400

    add js functionality to change background img

--- script/app.js
+++ script/app.js
@@ -1,17 +1,25 @@
 (() => {
-	// set up the puzzle pieces and boards
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+    // set up the puzzle pieces and boards
+    const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+        gameBoard = document.querySelector(".puzzle-board");
 
-	function changeImageSet() {
-		// change all the image elements on the page -> draggable image sources,
-		// and set the drop zone background
+    function changeImageSet() {
+        // change all the image elements on the page -> draggable image sources,
+        // let newBackgroundImage = `images/backGround${this.dataset.bgkey}.jpg`; (contenced down to one line)
+        //dynamically changes the background image by grabbing the data number using bgkey
 
-		debugger;
-	}
+        gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
+        // and set the drop zone background
 
-	// add event handling here -> how is the user going to use our app?
-	// what triggers do we need?
+        // debugger;
+    }
 
-	// click on the bottom buttons to change the puzzle image we're working with
-	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+    // add event handling here -> how is the user going to use our app?
+    // what triggers do we need?
+
+    // click on the bottom buttons to change the puzzle image we're working with
+    puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+    //research call, apply and bind
+    changeImageSet.call(puzzleButtons[0]); //empulates a click on the first button
 })();
