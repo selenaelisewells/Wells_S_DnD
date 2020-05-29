4b9b67a4c7dd5940b78b56cd96ac0ea99bc75381
Selena Wells <selena.elise.wells@gmail.com>
2020-05-29 13:00:12 -0400

    added drag and drop function

--- script/app.js
+++ script/app.js
@@ -1,7 +1,11 @@
 (() => {
     // set up the puzzle pieces and boards
     const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+        puzzlePieces = document.querySelectorAll('.puzzle-image'),
+        dropZones = document.querySelectorAll('.drop-zone'),
         gameBoard = document.querySelector(".puzzle-board");
+    // add event handling here -> how is the user going to use our app?
+    // what triggers do we need?
 
     function changeImageSet() {
         // change all the image elements on the page -> draggable image sources,
@@ -9,17 +13,35 @@
         //dynamically changes the background image by grabbing the data number using bgkey
 
         gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
-        // and set the drop zone background
+    } // and set the drop zone background
 
-        // debugger;
+    // debugger;
+    function allowDrag(event) {
+        console.log('started dragging an image: this one - ', event.target.id);
+
+        //let the drag happen and stor a reference of the ID8 of the element we're dragging
     }
 
-    // add event handling here -> how is the user going to use our app?
-    // what triggers do we need?
+    function allowDragOver(event) {
+        event.preventDefault(); //for next week
+        console.log('dragged something over me')
+    }
+
+    function allowDrop(event) {
+        console.log('dropped something on me')
+    }
+
+
 
     // click on the bottom buttons to change the puzzle image we're working with
     puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
 
+    puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
+
+    dropZones.forEach(zone => {
+        zone.addEventListener('dragover', allowDragOver);
+        zone.addEventListener('drop', allowDrop);
+    });
     //research call, apply and bind
     changeImageSet.call(puzzleButtons[0]); //empulates a click on the first button
 })();
